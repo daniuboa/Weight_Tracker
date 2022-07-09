f5a6cec4c2d152ecd4d85397071cc002c43c5f46
Dani Uboa <uboadaniel@gmail.com>
2022-07-09 18:32:35 +0300

    Add HTML structure

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,62 @@
 <script setup>
-  import { ref, shallowRef, computed, watch, nextTick } from 'vue'
-  import Chart from 'chart.js/auto'
+import { ref, shallowRef, computed, watch, nextTick } from "vue";
+import Chart from "chart.js/auto";
 
-  const weights = ref([])
+const weights = ref([]);
 
-  const weightChartEl = ref(null)
+const weightChartEl = ref(null);
 
-  const weightChart = shallowRef(null)
+const weightChart = shallowRef(null);
 
-  const weightInput = ref(60.0)
+const weightInput = ref(60.0);
 
-  const currentWeight = computed(() => {
-    return weights.value.sort((a, b) => b.date - a.date)[0] || { weight: 0 }
-  })
+const currentWeight = computed(() => {
+  return weights.value.sort((a, b) => b.date - a.date)[0] || { weight: 0 };
+});
 
 const addWeight = () => {
   weights.value.push({
-      weight: weightInput.value,
-      date: new Date().getTime
-    })
-  }
-
+    weight: weightInput.value,
+    date: new Date().getTime,
+  });
+};
 </script>
 
 <template>
-  <h1>Hello world!</h1>
+  <main>
+    <h1>Weight Tracker</h1>
+
+    <div class="current">
+      <span>{{ currentWeight.weight }}</span>
+      <small>Current weight (kg)</small>
+    </div>
+
+    <form @submit.prevent="addWeight">
+      <input type="number" step="0.1" v-model="weightInput" />
+      <input type="submit" value="Add Weight" />
+    </form>
+
+    <div v-if="weights && weights.length > 0">
+      <h2>Last Seven Days</h2>
+
+      <div class="canvas-box">
+        <canvas ref="weightChartEl"></canvas>
+      </div>
+
+      <div class="weight-history">
+        <h2>Weight History</h2>
+        <ul>
+          <li v-for="weight in weights">
+            <span> {{ weight.weight }}kg </span>
+            <small>
+              {{ new Date(weight.date).toLocaleDateString() }}
+            </small>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style>
-
 </style>
